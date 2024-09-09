6f3613beda7af37c461eb5fdecbffe011d57403c
Haseeb Ur Rehman <kamranking786500@gmail.com>
2024-09-10 01:54:34 +0500

    ui-100%-completed

---
+++ main.ts
@@ -0,0 +1,8 @@
+let userName = document.querySelector('#userName') as HTMLInputElement
+let generateBtn = document.querySelector('#genrtBtn') as HTMLButtonElement
+let form = document.querySelector('#form_section') as HTMLFormElement
+
+
+form.addEventListener('submit', event => {
+    event.preventDefault()
+})
